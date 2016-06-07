8357ff26a18380fcfd7185828cd282d7135c011d
Ekaterina Goranova <epgoranova@gmail.com>
2016-06-07 17:29:58 +0300

    Compute nullable, firstPos, lastPos, followPos

--- tree.go
+++ tree.go
@@ -1,76 +1,143 @@
 package relations
 
 import (
+	"github.com/deckarep/golang-set"
 	"github.com/oleiade/lane"
 )
 
 type treeNode struct {
-	data  string
-	left  *treeNode
-	right *treeNode
+	data     string
+	left     *treeNode
+	right    *treeNode
+	index    int // only for leaf nodes
+	nullable bool
+	firstPos mapset.Set
+	lastPos  mapset.Set
 }
 
-func parseTree(raw string) *treeNode {
+func (node *treeNode) updateFollowPos(followPos map[int]mapset.Set) map[int]mapset.Set {
+	switch node.data {
+	case ".":
+		for pos := range node.left.lastPos.Iter() {
+			position := pos.(int)
+			if followSet, ok := followPos[position]; ok {
+				followPos[position] = followSet.Union(node.right.firstPos)
+			} else {
+				followPos[position] = node.right.firstPos.Clone()
+			}
+		}
+	case "*":
+		for pos := range node.lastPos.Iter() {
+			position := pos.(int)
+			if followSet, ok := followPos[position]; ok {
+				followPos[position] = followSet.Union(node.firstPos)
+			} else {
+				followPos[position] = node.firstPos.Clone()
+			}
+		}
+	}
+	return followPos
+}
+
+func newLeafNode(data string, index int) *treeNode {
+	return &treeNode{
+		data:     data,
+		index:    index,
+		firstPos: mapset.NewSetWith(index),
+		lastPos:  mapset.NewSetWith(index),
+	}
+}
+
+func newOperatorNode(operator string, left, right *treeNode) *treeNode {
+	node := &treeNode{data: operator, left: left, right: right}
+
+	switch operator {
+	case "*":
+		node.nullable = true
+		node.firstPos = node.left.firstPos.Clone()
+		node.lastPos = node.left.lastPos.Clone()
+	case "+":
+		node.nullable = node.right.nullable || node.left.nullable
+		node.firstPos = node.left.firstPos.Union(node.right.firstPos)
+		node.lastPos = node.left.lastPos.Union(node.right.lastPos)
+	case ".":
+		node.nullable = node.right.nullable && node.left.nullable
+		if node.left.nullable {
+			node.firstPos = node.left.firstPos.Union(node.right.firstPos)
+		} else {
+			node.firstPos = node.left.firstPos.Clone()
+		}
+		if node.right.nullable {
+			node.lastPos = node.left.lastPos.Union(node.right.lastPos)
+		} else {
+			node.lastPos = node.right.lastPos.Clone()
+		}
+	}
+	return node
+}
+
+func parseTree(raw string) (*treeNode, map[int]mapset.Set) {
+	followPos := make(map[int]mapset.Set)
+
 	nodeStack := lane.NewStack()
 	operatorStack := lane.NewStack()
 
-	index := 0
+	position := 0
+	index := 1
 
-	for index < len(raw) {
-		char := string(raw[index])
+	for position < len(raw) {
+		char := string(raw[position])
 		switch char {
 		case "<":
-			// TODO: separate pairs and index
-			var number string
-			number += char
+			// TODO: separate pairs and position
+			// check if both are epsilon - in that case the node is nullable
+			var leaf string
+			leaf += char
 			for char != ">" {
-				index += 1
-				char = string(raw[index])
-				number += char
+				position += 1
+				char = string(raw[position])
+				leaf += char
 			}
-			node := &treeNode{data: number}
-			nodeStack.Push(node)
 
-		case "(":
+			nodeStack.Push(newLeafNode(leaf, index))
+			index += 1
+
+		case "(", "+", ".":
 			operatorStack.Push(char)
 
 		case ")":
-			operator := operatorStack.Pop()
+			operator := operatorStack.Pop().(string)
 			for operator != "(" {
-				right := nodeStack.Pop()
-				left := nodeStack.Pop()
-				node := &treeNode{
-					data: operator.(string),
-					right: right.(*treeNode),
-					left: left.(*treeNode),
-				}
-				nodeStack.Push(node)
-				operator = operatorStack.Pop()
+				right := nodeStack.Pop().(*treeNode)
+				left := nodeStack.Pop().(*treeNode)
+				newNode := newOperatorNode(operator, left, right)
+				nodeStack.Push(newNode)
+				followPos = newNode.updateFollowPos(followPos)
+				operator = operatorStack.Pop().(string)
 			}
 
 		case "*":
-			operand := nodeStack.Pop()
-			node := &treeNode{data: char, left: operand.(*treeNode)}
-			nodeStack.Push(node)
-
-		case "+", ".":
-			operatorStack.Push(char)
+			operand := nodeStack.Pop().(*treeNode)
+			newNode := newOperatorNode(char, operand, nil)
+			nodeStack.Push(newNode)
+			followPos = newNode.updateFollowPos(followPos)
 		}
-		index += 1
+		position += 1
 	}
 
+	// add endmarker character
+	operatorStack.Push(".")
+	nodeStack.Push(newLeafNode("!", index))
+
 	for !operatorStack.Empty() {
-		operator := operatorStack.Pop()
-		right := nodeStack.Pop()
-		left := nodeStack.Pop()
-		node := &treeNode{
-			data: operator.(string),
-			left: left.(*treeNode),
-			right: right.(*treeNode),
-		}
-		nodeStack.Push(node)
+		operator := operatorStack.Pop().(string)
+		right := nodeStack.Pop().(*treeNode)
+		left := nodeStack.Pop().(*treeNode)
+		newNode := newOperatorNode(operator, left, right)
+		nodeStack.Push(newNode)
+		followPos = newNode.updateFollowPos(followPos)
 	}
 
 	root := nodeStack.Pop()
-	return root.(*treeNode)
+	return root.(*treeNode), followPos
 }
--- tree_test.go
+++ tree_test.go
@@ -2,12 +2,14 @@ package relations
 
 import (
 	"testing"
+
+	"github.com/deckarep/golang-set"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParseTreeBuilder(t *testing.T) {
 	regex := `(<a,>+<b,>)*.<a,>.<b,>.<b,>`
-	root := parseTree(regex)
+	root, _ := parseTree(regex)
 
 	assert.Equal(t, root.data, ".")
 	assert.Equal(t, root.left.data, "*")
@@ -21,5 +23,87 @@ func TestParseTreeBuilder(t *testing.T) {
 	assert.Equal(t, root.right.right.data, ".")
 
 	assert.Equal(t, root.right.right.left.data, "<b,>")
-	assert.Equal(t, root.right.right.right.data, "<b,>")
+	assert.Equal(t, root.right.right.right.data, ".")
+
+	assert.Equal(t, root.right.right.right.left.data, "<b,>")
+	assert.Equal(t, root.right.right.right.right.data, "!")
+}
+
+func TestParseTreeIndexing(t *testing.T) {
+	regex := `(<a,>+<b,>)*.<a,>.<b,>.<b,>`
+	root, _ := parseTree(regex)
+
+	assert.Equal(t, root.left.left.left.index, 1)
+	assert.Equal(t, root.left.left.right.index, 2)
+	assert.Equal(t, root.right.left.index, 3)
+	assert.Equal(t, root.right.right.left.index, 4)
+	assert.Equal(t, root.right.right.right.left.index, 5)
+	assert.Equal(t, root.right.right.right.right.index, 6)
+}
+
+func TestTreeNodeNullable(t *testing.T) {
+	regex := `(<a,>+<b,>)*.<a,>.<b,>.<b,>`
+	root, _ := parseTree(regex)
+
+	assert.True(t, root.left.nullable)
+	assert.False(t, root.nullable)
+	assert.False(t, root.right.nullable)
+}
+
+func TestFirstPos(t *testing.T) {
+	regex := `(<a,>+<b,>)*.<a,>.<b,>.<b,>`
+	root, _ := parseTree(regex)
+
+	assert.True(t, root.firstPos.Equal(mapset.NewSetWith(1, 2, 3)))
+	assert.True(t, root.left.firstPos.Equal(mapset.NewSetWith(1, 2)))
+	assert.True(t, root.right.firstPos.Equal(mapset.NewSetWith(3)))
+
+	assert.True(t, root.left.left.firstPos.Equal(mapset.NewSetWith(1, 2)))
+	assert.True(t, root.left.left.left.firstPos.Equal(mapset.NewSetWith(1)))
+	assert.True(t, root.left.left.right.firstPos.Equal(mapset.NewSetWith(2)))
+
+	assert.True(t, root.right.left.firstPos.Equal(mapset.NewSetWith(3)))
+	assert.True(t, root.right.right.firstPos.Equal(mapset.NewSetWith(4)))
+
+	assert.True(t, root.right.right.left.firstPos.Equal(mapset.NewSetWith(4)))
+	assert.True(t, root.right.right.right.firstPos.Equal(mapset.NewSetWith(5)))
+
+	assert.True(t, root.right.right.right.left.firstPos.Equal(mapset.NewSetWith(5)))
+	assert.True(t, root.right.right.right.right.firstPos.Equal(mapset.NewSetWith(6)))
+}
+
+func TestLastPos(t *testing.T) {
+	regex := `(<a,>+<b,>)*.<a,>.<b,>.<b,>`
+	root, _ := parseTree(regex)
+
+	assert.True(t, root.lastPos.Equal(mapset.NewSetWith(6)))
+	assert.True(t, root.left.lastPos.Equal(mapset.NewSetWith(1, 2)))
+	assert.True(t, root.right.lastPos.Equal(mapset.NewSetWith(6)))
+
+	assert.True(t, root.left.left.lastPos.Equal(mapset.NewSetWith(1, 2)))
+	assert.True(t, root.left.left.left.lastPos.Equal(mapset.NewSetWith(1)))
+	assert.True(t, root.left.left.right.lastPos.Equal(mapset.NewSetWith(2)))
+
+	assert.True(t, root.right.left.lastPos.Equal(mapset.NewSetWith(3)))
+	assert.True(t, root.right.right.lastPos.Equal(mapset.NewSetWith(6)))
+
+	assert.True(t, root.right.right.left.lastPos.Equal(mapset.NewSetWith(4)))
+	assert.True(t, root.right.right.right.lastPos.Equal(mapset.NewSetWith(6)))
+
+	assert.True(t, root.right.right.right.left.lastPos.Equal(mapset.NewSetWith(5)))
+	assert.True(t, root.right.right.right.right.lastPos.Equal(mapset.NewSetWith(6)))
+}
+
+func TestFollowPos(t *testing.T) {
+	regex := `(<a,>+<b,>)*.<a,>.<b,>.<b,>`
+	_, followPos := parseTree(regex)
+
+	assert.True(t, followPos[1].Equal(mapset.NewSetWith(1, 2, 3)))
+	assert.True(t, followPos[2].Equal(mapset.NewSetWith(1, 2, 3)))
+	assert.True(t, followPos[3].Equal(mapset.NewSetWith(4)))
+	assert.True(t, followPos[4].Equal(mapset.NewSetWith(5)))
+	assert.True(t, followPos[5].Equal(mapset.NewSetWith(6)))
+
+	_, isSet := followPos[6]
+	assert.False(t, isSet)
 }
