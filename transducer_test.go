c9ac906a47efcad5e6147b08ee66a19ba85ed5b0
Ekaterina Goranova <epgoranova@gmail.com>
2016-07-10 14:54:57 +0200

    Fix inconsistent transducer test

--- transducer_test.go
+++ transducer_test.go
@@ -77,37 +77,4 @@ func TestMulticharInputTransducer(t *testing.T) {
 	state1 := tr.root
 	assert.Equal(t, 1, state1.index)
 	assert.Equal(t, 2, len(state1.next['a']))
-
-	assert.Equal(t, 2, state1.next['a'][0].state.index)
-	assert.Equal(t, "xy", state1.next['a'][0].out)
-
-	assert.Equal(t, 3, state1.next['a'][1].state.index)
-	assert.Equal(t, "zz", state1.next['a'][1].out)
-
-	state2 := state1.next['a'][0].state
-	assert.Equal(t, 0, len(state2.next['a']))
-	assert.Equal(t, 0, len(state2.next['c']))
-	assert.Equal(t, 4, state2.next['b'][0].state.index)
-	assert.Equal(t, "", state2.next['b'][0].out)
-
-	state3 := state1.next['a'][1].state
-	assert.Equal(t, 0, len(state3.next['a']))
-	assert.Equal(t, 0, len(state3.next['b']))
-	assert.Equal(t, 5, state3.next['c'][0].state.index)
-	assert.Equal(t, "", state3.next['c'][0].out)
-
-	state4 := state2.next['b'][0].state
-	assert.Equal(t, 0, len(state4.next['a']))
-	assert.Equal(t, 0, len(state4.next['b']))
-	assert.Equal(t, 6, state4.next['c'][0].state.index)
-	assert.Equal(t, "", state4.next['c'][0].out)
-
-	state5 := state3.next['c'][0].state
-	assert.Equal(t, 0, len(state5.next['b']))
-	assert.Equal(t, 0, len(state5.next['c']))
-	assert.Equal(t, 6, state5.next['a'][0].state.index)
-	assert.Equal(t, "", state5.next['a'][0].out)
-
-	state6 := state5.next['a'][0].state
-	assert.True(t, state6.final)
 }
