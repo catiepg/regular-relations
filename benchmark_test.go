03be8ff4ccdb89478088032e25b4097f31ef2ad0
Ekaterina Goranova <epgoranova@gmail.com>
2016-09-12 21:48:25 +0200

    Refactor benchmark test

--- benchmark_test.go
+++ benchmark_test.go
@@ -6,35 +6,43 @@ import (
 	"testing"
 )
 
-var alphabet = []rune("abcdefghipqrstuvwxyz")
-
+// writeRandomTo writes a random rune to the given buffer.
 func writeRandomTo(b *bytes.Buffer) {
+	var alphabet = []rune("abcdefghipqrstuvwxyz")
 	n := rand.Intn(10) + 5
-
 	for i := 0; i < n; i++ {
 		b.WriteRune(alphabet[rand.Intn(len(alphabet))])
 	}
 }
 
-func newSubTransducer(pairCount int) *Subsequential {
+// regularRelationExpr generates a random regular relation expression
+// according to the following template:
+// 		<string, string>+<string, string>+<string, string>+...
+// The parameter specifies the number of pairs to be generated.
+func regularRelationExpr(pairs int) *bytes.Buffer {
 	var b bytes.Buffer
 
-	for i := 1; i <= pairCount; i++ {
+	for i := 1; i <= pairs; i++ {
 		b.WriteRune('<')
 		writeRandomTo(&b)
 		b.WriteRune(',')
 		writeRandomTo(&b)
 		b.WriteRune('>')
 
-		if i != pairCount {
+		if i != pairs {
 			b.WriteRune('+')
 		}
 	}
-
-	sub, _ := NewSubsequential(&b)
-	return sub
+	return &b
 }
 
-func TestLargeInput(t *testing.T) {
-	newSubTransducer(200)
+func benchmarkRelations(b *testing.B, pairs int) {
+	for n := 0; n < b.N; n++ {
+		Build(regularRelationExpr(pairs))
+	}
 }
+
+func BenchmarkRelations10(b *testing.B)    { benchmarkRelations(b, 10) }
+func BenchmarkRelations100(b *testing.B)   { benchmarkRelations(b, 100) }
+func BenchmarkRelations1000(b *testing.B)  { benchmarkRelations(b, 1000) }
+func BenchmarkRelations10000(b *testing.B) { benchmarkRelations(b, 10000) }
